731e2319b72a8356e4efd799af0968db5d5fe304
himanshuraw <himanshurawat180@gmail.com>
2024-11-21 11:30:54 +0530

    Initialized Project

---
+++ src/main/java/com/himanshu/academicerp/AcademicErpApplication.java
@@ -0,0 +1,13 @@
+package com.himanshu.academicerp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcademicErpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AcademicErpApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/himanshu/academicerp/AcademicErpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.himanshu.academicerp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcademicErpApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
